d210d49b5ee442ca5257921a9c3f7250930c20d3
L!nuxGeek <99080650+Linuxgys@users.noreply.github.com>
2025-07-25 16:39:32 +0000

    tfbackedn

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = "ap-south-1" # Region where your AWS resources will be deployed
+}
+
+terraform {
+  backend "s3" {
+    bucket         = "tf-backup-ajeet"                # ✅ Replace with your actual S3 bucket name
+    key            = "terraform/dev/your-project.tfstate" # ✅ State file path inside the bucket
+    region         = "ap-south-1"                     # ✅ Same region as your S3 bucket and DynamoDB table
+    dynamodb_table = "terraform-lock-table"           # ✅ Replace with your DynamoDB locking table name
+    encrypt        = true                             # ✅ Encrypt the state file at rest
+  }
 }
