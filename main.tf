2874586d88e64ef542898a5292a634c439222c70
L!nuxGeek <99080650+Linuxgys@users.noreply.github.com>
2025-07-28 12:24:41 +0000

    latest

--- main.tf
+++ main.tf
@@ -7,6 +7,8 @@ terraform {
     bucket         = "tf-backup-ajeet"                    # ✅ Replace with your actual S3 bucket name
     key            = "terraform/dev/your-project.tfstate" # ✅ State file path inside the bucket
     region         = "ap-south-1"                         # ✅ Same region as your S3 bucket and DynamoDB table
+    #use_lock_table = true     
+    #lock_table     = "terraform-lock-table"
     dynamodb_table = "terraform-lock-table"               # ✅ Replace with your DynamoDB locking table name
     encrypt        = true                                 # ✅ Encrypt the state file at rest
   }
