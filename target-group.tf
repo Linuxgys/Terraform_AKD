0ce1c82e15d1cc5d2328454a3f4148be380d02cd
L!nuxGeek <99080650+Linuxgys@users.noreply.github.com>
2025-07-28 16:09:11 +0000

    SSL Deployment 443 lisner set 2.1

--- target-group.tf
+++ target-group.tf
@@ -5,7 +5,7 @@ resource "aws_lb_target_group" "web_tg" {
   vpc_id   = aws_vpc.main_vpc.id
 
   health_check {
-    path                = "/health"
+    path                = "/health" # Make sure your app responds with 200 on /health
     matcher             = "200"
     interval            = 30
     timeout             = 5
@@ -13,8 +13,9 @@ resource "aws_lb_target_group" "web_tg" {
     unhealthy_threshold = 2
   }
 }
+
 resource "aws_lb_target_group_attachment" "web_attachment" {
   target_group_arn = aws_lb_target_group.web_tg.arn
-  target_id        = aws_instance.Web_test[0].id # Replace with your actual EC2 resource name
+  target_id        = aws_instance.Web_test.id # No [0] unless using count or for_each
   port             = 80
 }
