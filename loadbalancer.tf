c7ced3dc10eff4e59a881869d0909c94588409cc
L!nuxGeek <99080650+Linuxgys@users.noreply.github.com>
2025-07-28 17:15:54 +0000

    add ec2 count for test2.0

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -26,5 +26,32 @@ resource "aws_lb_listener" "http_redirect" {
     }
   }
 }
+resource "aws_lb_listener" "https_listener" {
+  load_balancer_arn = aws_lb.web_alb.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-Res-2021-06"
 
+  certificate_arn = "arn:aws:acm:ap-south-1:040768515690:certificate/092221bc-5179-488e-b541-d8d9faaded98"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.web_tg.arn
+  }
+}
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
 
