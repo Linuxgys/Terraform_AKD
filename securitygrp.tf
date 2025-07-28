480c50dbd252fc09f5efe9768628b40e0fb7c559
L!nuxGeek <99080650+Linuxgys@users.noreply.github.com>
2025-07-28 14:25:44 +0000

    SSL Deployment fixed3

--- securitygrp.tf
+++ securitygrp.tf
@@ -17,7 +17,6 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # Allow HTTPS directly in SG (optional if you prefer rules separately)
   ingress {
     from_port   = 443
     to_port     = 443
@@ -35,24 +34,4 @@ resource "aws_security_group" "web_sg" {
   tags = {
     Name = "WebSecurityGroup"
   }
-}
-
-# If you still want separate rules for visibility:
-
-resource "aws_security_group_rule" "alb_http" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.web_sg.id
-}
-
-resource "aws_security_group_rule" "alb_https" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.web_sg.id
 }
