71f1991e7a30c4a00e8c61f8c99d7093111b98f8
L!nuxGeek <99080650+Linuxgys@users.noreply.github.com>
2025-07-28 15:27:35 +0000

    SSL Deployment 443

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -11,15 +11,18 @@ resource "aws_lb" "web_alb" {
   enable_deletion_protection = false
 }
 
-resource "aws_lb_listener" "https_listener" {
-  load_balancer_arn = aws_lb.web_alb.arn
-  port              = 443
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-Res-2021-06"
-  certificate_arn   = "arn:aws:acm:ap-south-1:040768515690:certificate/092221bc-5179-488e-b541-d8d9faaded98"
-
+resource "aws_lb_listener" "http_redirect" {
+  load_balancer_arn = aws_lb.alb.arn # Replace with your ALB resource name
+  port              = 80
+  protocol          = "HTTP"
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.web_tg.arn
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
+
